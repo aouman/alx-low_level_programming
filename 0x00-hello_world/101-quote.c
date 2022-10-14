88f443bba0b72b08a62e06dd0a67f33deefe2a1d
aouman <k2.aouman@gmail.com>
2022-10-14 07:34:18 -0700

    coreption task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main- Entry point
- * @void: parameter
- * Return: 1 (success)
+ * main - Entry point
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 
 {
 
-	fprintf(stderr, "and that piece of art is useful\" - \				Dora Korpar, 2015-10-19\n");
-	return (1);
+write(STDOUT_FILENO, "and that piece of art is useful
+	\" - Dora Korpar, 2015-10-19\n", 59);
 
+return (1);
 }
