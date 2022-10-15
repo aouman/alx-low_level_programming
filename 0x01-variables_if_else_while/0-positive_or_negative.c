e413e7a68f58d82971f051d0b2204a4ab233a5d1
aouman <k2.aouman@gmail.com>
2022-10-15 04:53:41 -0700

    positif or negatif

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+	int n = 0;
+	scanf("%d", &n);
+	if(n>0){
+		printf("is positif\n");
+	}else if(n==0){
+		printf("is zero\n");
+	}else if (n<0) {
+		printf("is negative\n");
+	}
+	return 0;
+}
