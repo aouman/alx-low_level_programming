5550b9041d11e8cd4070ae50be201da5a8e92c53
aouman <k2.aouman@gmail.com>
2022-10-15 05:19:48 -0700

    add the call variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 int main(void)
 {
+	/*
+	 *I check the value of the varible n
+	 * */
+
 	int n = 0;
 	scanf("%d", &n);
 
-	if(n > 0)
+	if (n > 0)
 	{
-		printf("is positive\n");
-	}else if(n == 0)
+		printf("%d is positive\n", n);
+	} else if (n == 0)
 	{
-		printf("is zero\n");
-	}else if (n < 0)
+		printf("%d is zero\n", n);
+	} else if (n < 0)
 	{
-		printf("is negative\n");
+		printf("-%d is negative\n", n);
 	}
 	return (0);
 }
