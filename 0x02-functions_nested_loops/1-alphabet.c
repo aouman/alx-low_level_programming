373c2bbf3002ce1007d64660ce38f8799cbaad37
aouman <k2.aouman@gmail.com>
2022-10-19 06:38:24 -0700

    test 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+		putchar('\n');
+	}
+	return (0);
+}
