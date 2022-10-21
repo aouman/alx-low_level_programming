ec7d1b9e61fa4b7a76a8314c928ea6ddc469aa74
aouman <k2.aouman@gmail.com>
2022-10-21 07:11:05 -0700

    a function that checks for uppercase character.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,20 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  */
-int print_alphabet(void)
 
+void print_alphabet(void)
 {
-	char n;
+	char ch;
 
-	for (n = 'a'; n <= 'z'; n++)
-	_putchar(n);
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
 	_putchar('\n');
+
 }
